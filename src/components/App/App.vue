51dbc825183ea777f5352be233dad353012db714
Alia Peterson <alia@pnwebs.com>
2021-02-24 15:50:22 -0700

    Create header component

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from '../Header/Header'
 
 export default {
   name: 'App',
   components: {
-
+    Header
   }
 }
 </script>
