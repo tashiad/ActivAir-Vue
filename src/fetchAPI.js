cbdbf01d041d5cb066f31ace44dd9aab88a1b988
Alia Peterson <alia@pnwebs.com>
2021-02-27 16:24:10 -0700

    Update API key with new value

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,24 +1,26 @@
+const key = '2ee2ed8e-4950-4027-9dfb-a2c0e35f3218'
+
 export default {
   getStates () {
-    return fetch('http://api.airvisual.com/v2/states?country=USA&key=ef754ecf-5ebf-47b6-ae9f-f96d1f8eda21')
+    return fetch(`http://api.airvisual.com/v2/states?country=USA&key=${key}`)
       .then(response => response.json())
       .catch(error => console.log(error))
   },
 
   getCities (state) {
-    return fetch(`http://api.airvisual.com/v2/cities?state=${state}&country=USA&key=ef754ecf-5ebf-47b6-ae9f-f96d1f8eda21`)
+    return fetch(`http://api.airvisual.com/v2/cities?state=${state}&country=USA&key=${key}`)
       .then(response => response.json())
       .catch(error => console.log(error))
   },
 
   getLocale (city, state) {
-    return fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=ef754ecf-5ebf-47b6-ae9f-f96d1f8eda21`)
+    return fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=${key}`)
       .then(response => response.json())
       .catch(error => console.log(error))
   },
 
   getCurrent () {
-    return fetch('http://api.airvisual.com/v2/nearest_city?key=ef754ecf-5ebf-47b6-ae9f-f96d1f8eda21')
+    return fetch(`http://api.airvisual.com/v2/nearest_city?key=${key}`)
       .then(response => response.json())
       .catch(error => console.log(error))
   }
