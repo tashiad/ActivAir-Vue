1e71fa77bc2d6e68d2ba7c6757fe5f7f4c03ca71
Alia Peterson <alia@pnwebs.com>
2021-03-01 11:06:46 -0700

    Refactor to include beforeeach

--- cypress/integration/activair_spec.js
+++ cypress/integration/activair_spec.js
@@ -1,19 +1,25 @@
-const baseUrl = 'http://localhost:8080/'
-const key = '643484ca-9138-4647-b058-430c60bdb96f'
-const city = 'Denver'
-const state = 'Colorado'
-const citiesAPI = `http://api.airvisual.com/v2/cities?state=${state}&country=USA&key=${key}`
-const statesAPI = `http://api.airvisual.com/v2/states?country=USA&key=${key}`
-const cityStateAPI = `http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=${key}`
-const currentLocationAPI = `http://api.airvisual.com/v2/nearest_city?key=${key}`
-
 describe('ActivAir Website', () => {
-  it('Should display a header with website name and subheader', () => {
-    cy.intercept('GET', statesAPI, { fixture: 'state-data' })
+  const baseUrl = 'http://localhost:8080/'
+  const key = '643484ca-9138-4647-b058-430c60bdb96f'
+  const citiesAPI = `http://api.airvisual.com/v2/cities?state=Colorado&country=USA&key=${key}`
+  const statesAPI = `http://api.airvisual.com/v2/states?country=USA&key=${key}`
+  const cityStateAPI = `http://api.airvisual.com/v2/city?city=Denver&state=Colorado&country=USA&key=${key}`
+  const currentLocationAPI = `http://api.airvisual.com/v2/nearest_city?key=${key}`
+
+  beforeEach(() => {
+    cy.intercept('GET', statesAPI, { fixture: 'state-data.json' })
       .visit(baseUrl)
-      .get('header')
-        .get('h1').should('have.text', 'ActivAir')
-        .get('h2').should('have.text', 'Find fresh air for your outdoor activities.')
   })
 
+  it('Should display a header with website name and subheader', () => {
+    cy.get('header')
+      .get('h1').should('have.text', 'ActivAir')
+      .get('h2').should('have.text', 'Find fresh air for your outdoor activities.')
+  })
+
+  it('Should populate state and city dropdowns with relevant information', () => {
+    cy.get('select#dropdown-states').select('Colorado')
+      .intercept('GET', citiesAPI, { fixture: 'city-data.json' })
+      .get('select#dropdown-cities').select('Denver').should('have.value', 'Denver')
+  })
 })
