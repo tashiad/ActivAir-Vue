b9eb2946a4c0cfb69496b4778fd7a76fc1c41e48
Alia Peterson <alia@pnwebs.com>
2021-03-02 14:17:51 -0700

    Add s to http fetch urls

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -2,22 +2,22 @@ import key from './keyAPI'
 
 export default {
   getStates () {
-    return fetch(`http://api.airvisual.com/v2/states?country=USA&key=${key}`)
+    return fetch(`https://api.airvisual.com/v2/states?country=USA&key=${key}`)
       .then(response => response.json())
   },
 
   getCities (state) {
-    return fetch(`http://api.airvisual.com/v2/cities?state=${state}&country=USA&key=${key}`)
+    return fetch(`https://api.airvisual.com/v2/cities?state=${state}&country=USA&key=${key}`)
       .then(response => response.json())
   },
 
   getLocale (city, state) {
-    return fetch(`http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=${key}`)
+    return fetch(`https://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=${key}`)
       .then(response => response.json())
   },
 
   getCurrent () {
-    return fetch(`http://api.airvisual.com/v2/nearest_city?key=${key}`)
+    return fetch(`https://api.airvisual.com/v2/nearest_city?key=${key}`)
       .then(response => response.json())
   }
 }
